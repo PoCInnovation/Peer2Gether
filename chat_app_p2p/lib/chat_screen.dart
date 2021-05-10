be2ad8cb36b667f469ac46252de0772ffc1e93fc
EpitAlexandre <alexandre.collin-betheuil@epitech.eu>
2021-05-11 01:26:42 +0200

    [ADD] end of application

--- chat_app_p2p/lib/chat_screen.dart
+++ chat_app_p2p/lib/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:chat_app_p2p/user_model.dart';
+import 'package:chat_app_p2p/message_model.dart';
 
 class ChatScreen extends StatefulWidget {
   final User user;
@@ -11,8 +12,166 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  _chatBubble(Message message, bool isMe, bool isSameUser) {
+    if (isMe) {
+      return Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topRight,
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.80,
+              ),
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(vertical: 10),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+              child: Text(
+                message.text,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          !isSameUser
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      message.time,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black45,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              : Container(
+                  child: null,
+                ),
+        ],
+      );
+    } else {
+      return Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topLeft,
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.80,
+              ),
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(vertical: 10   ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
+              child: Text(
+                message.text,
+                style: TextStyle(
+                  color: Colors.black54,
+                ),
+              ),
+            ),
+          ),
+          !isSameUser
+              ? Row(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      message.time,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black45,
+                      ),
+                    ),
+                  ],
+                )
+              : Container(
+                  child: null,
+                ),
+        ],
+      );
+    }
+  }
+
+  _sendMessageArea() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      height: 70,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration.collapsed(
+                hintText: 'Send a message...',
+              ),
+              textCapitalization: TextCapitalization.sentences,
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            iconSize: 25,
+            color: Theme.of(context).primaryColor,
+            onPressed: () {
+              //send message
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    int prevUserId;
     return Scaffold(
       backgroundColor: Color(0xFFF6F6F6),
       appBar: AppBar(
@@ -22,16 +181,33 @@ class _ChatScreenState extends State<ChatScreen> {
           textAlign: TextAlign.center,
           text: TextSpan(
             children: [
-              TextSpan(text: widget.user.name),
-              TextSpan(text: '\n'),
               TextSpan(
-                  text: widget.user.isOnline ? 'Online' : 'Offline',
-                  style: TextStyle(fontSize: 11, fontWeight: FontWeight.w400))
+                  text: widget.user.name,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  )),
+              TextSpan(text: '\n'),
+              widget.user.isOnline
+                  ? TextSpan(
+                      text: 'Online',
+                      style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    )
+                  : TextSpan(
+                      text: 'Offline',
+                      style: TextStyle(
+                        fontSize: 11,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    )
             ],
           ),
         ),
         leading: IconButton(
-            icon: Icon(Icons.arrow_back),
+            icon: Icon(Icons.arrow_back_ios),
             color: Colors.white,
             onPressed: () {
               Navigator.pop(context);
@@ -40,45 +216,20 @@ class _ChatScreenState extends State<ChatScreen> {
       body: Column(
         children: <Widget>[
           Expanded(
-            child: Column(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Container(
-                      child: Container(
-                        alignment: Alignment.topLeft,
-                        constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.8,
-                        ),
-                        child: Text('gg for the Ultron fight'),
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(15),
-                          boxShadow: [BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2,
-                            blurRadius: 5
-                          ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  child: Text('data'),
-                ),
-                Container(
-                  child: Text('data'),
-                ),
-              ],
+            child: ListView.builder(
+              reverse: true,
+              padding: EdgeInsets.all(20),
+              itemCount: messages.length,
+              itemBuilder: (BuildContext context, int index) {
+                final Message message = messages[index];
+                final bool isMe = message.sender.id == currentUser.id;
+                final bool isSameUser = prevUserId == message.sender.id;
+                prevUserId = message.sender.id;
+                return _chatBubble(message, isMe, isSameUser);
+              },
             ),
           ),
-          Container(
-            child: Text('Send'),
-          ),
+          _sendMessageArea(),
         ],
       ),
     );
--- chat_app_p2p/lib/user_model.dart
+++ chat_app_p2p/lib/user_model.dart
@@ -1,34 +1,27 @@
 class User {
   final int id;
   final String name;
-  final String image;
+  final String imageUrl;
   final bool isOnline;
 
   User({
     this.id,
     this.name,
-    this.image,
+    this.imageUrl,
     this.isOnline,
   });
 }
 
 final User currentUser = User(
   id: 0,
-  name: "Alexandre",
-  image: "assets/poc.jpg  ",
+  name: 'PoC',
+  imageUrl: 'assets/poc.jpg',
   isOnline: true,
 );
 
 final User ironMan = User(
   id: 1,
-  name: "Iron Man",
-  image: "assets/iron-man.png",
-  isOnline: false,
-);
-
-final User PoC = User(
-  id: 1,
-  name: "PoC",
-  image: "assets/poc.jpg",
+  name: 'Iron Man',
+  imageUrl: 'assets/iron-man.png',
   isOnline: true,
 );
