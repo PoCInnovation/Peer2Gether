52d37fea80aded1b81c781dbceb73900f6923fb2
EpitAlexandre <alexandre.collin-betheuil@epitech.eu>
2021-07-14 22:13:06 +0200

    [ADD] App with firestore connexion

---
+++ peer_to_gether_app/build/barcode_scan/generated/source/buildConfig/debug/de/mintware/barcodescan/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.mintware.barcodescan;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "de.mintware.barcodescan";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ test_firestore/build/app/generated/source/buildConfig/debug/com/poc/test_firestore/test_firestore/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.poc.test_firestore.test_firestore;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.poc.peertogether";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
